f33321dafd294431f04ae089d4f43edfee992913
alozano33 <alozano33@gmail.com>
2025-05-24 23:49:34 -0300

    ajustes finais

--- src/main/kotlin/com/credit/simulator/creditsimulationservice/domain/SimulacaoEmprestimoRequest.kt
+++ src/main/kotlin/com/credit/simulator/creditsimulationservice/domain/SimulacaoEmprestimoRequest.kt
@@ -3,6 +3,13 @@ package com.credit.simulator.creditsimulationservice.domain
 import java.math.BigDecimal
 import java.time.LocalDate
 
+/**
+ * Classe de dados (data class) que representa uma requisição de simulação de empréstimo.
+ * 
+ * @property valorEmprestimo O valor do empréstimo solicitado em BigDecimal
+ * @property dataNascimento A data de nascimento do solicitante
+ * @property quantidadeParcelas O número de parcelas desejado para o empréstimo
+ */
 data class SimulacaoEmprestimoRequest(
     val valorEmprestimo: BigDecimal,
     val dataNascimento: LocalDate,
